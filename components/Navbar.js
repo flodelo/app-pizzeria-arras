159226bfb248720790f34c64945a74d81ea31f0c
Florian <florian.delobelle@gmail.com>
2023-01-27 11:19:30 +0100

    added project images

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css"
+
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+      Navbar
+    </div>
+  )
+}
